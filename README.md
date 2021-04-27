0650ffc809627d346394c657a46bd1ce2710f709
Lyon <lyonbeckers@gmail.com>
2021-04-27 16:54:19 -0600

    Add example, readme (#5)
    * use protobuf 2.22.0
    
    * example and readme
    
    * correct schema

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+![Build](https://github.com/gameroasters/atlas/workflows/CI/badge.svg)
+
 # atlas
 
-todo
+atlas is a rust library for the purpose of composing REST APIs out of re-usable and extensible modules, specifically with supporting the networking needs of online gaming services in mind.
+
+## How it works
+
+Structs which implement the `CustomModule` trait are joined by an object which implements the `CustomServer` trait, which dispatches the warp filters defined within the modules. `CustomModule`s can work on data through the use of "resources", which are arbitrary types that are stored in an `Hlist`.
+
+See the examples in the repo for more details.
---
+++ examples/custom_server.rs
@@ -0,0 +1,48 @@
+use std::sync::Arc;
+
+use atlas::*;
+
+struct MyServer {
+	resources: <Self as CustomServer>::Resources,
+}
+
+impl CustomServer for MyServer {
+	type Resources = Hlist![Arc<userlogin::UserLoginResource>];
+
+	const MODULES: &'static [Module<Self>] = &[Module {
+		name: "userlogin",
+		call: userlogin::UserLogin::create_filter,
+	}];
+
+	fn get_resources(&self) -> &Self::Resources {
+		&self.resources
+	}
+}
+
+impl ModuleResources<userlogin::UserLogin> for MyServer {
+	fn get_server_resources(
+		&self,
+	) -> <userlogin::UserLogin as CustomModule>::Resources {
+		let (reshaped, _) = self.get_resources().clone().sculpt();
+		reshaped
+	}
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let session_db =
+		Arc::new(userlogin::sessions::InMemorySessionDB::default());
+	let user_db = Arc::new(
+		userlogin::user::in_memory::InMemoryUserDB::default(),
+	);
+
+	let server = Arc::new(MyServer {
+		resources: hlist![Arc::new(
+			userlogin::UserLoginResource::new(session_db, user_db)
+		),],
+	});
+
+	atlas::initialize_server(server, ([0, 0, 0, 0], 8080)).await;
+
+	Ok(())
+}
